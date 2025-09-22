da107433a8b91f52b707eab0825aa9d31d7e082e
siva-sankaran <7930913+siva-sankaran@users.noreply.github.com>
2025-09-22 20:37:44 +0530

    add unit test proj for desktop app

---
+++ TicTacToeWindowsAppTest/GameBoardTest.cs
@@ -0,0 +1,25 @@
+﻿using TicTacToeWindowsApp;
+
+namespace TicTacToeWindowsAppTest
+{
+    public class GameBoardTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void GivenBoardWithTwoSameSignsInRow_WhenPlayer1SetsSameSign_ThenGameEndsWithPlayer1Winning(int row)
+        {
+            //Arrange
+            GameBoard board = new();
+            board.SetO(row, 0);
+            board.SetO(row, 1);
+
+            //Act
+            board.SetO(row, 2);
+
+            //Assert
+            Assert.True(board.IsWin(), "Game should be in winned state");
+        }
+    }
+}
